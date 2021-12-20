450d0b7b6a22d413227eecf4176d8d6fade4c66a
salvosanzone <salvosansone.ss@gmail.com>
2021-12-20 15:27:24 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+/* Snack 3
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post. */
+
+//creo l'array
+$movies = [
+  "14/09/1963" => [
+    [
+    "title" => "eight and half",
+    "author" => "federico fellini",
+    "text" => "text post 1"
+  ],
+  [
+    "title" => "i vitelloni",
+    "author" => "federico fellini",
+    "text" => "text post 2"
+  ]
+  ],
+  "14/09/1931" => [
+    [
+      "title" => "il mostro di dusseldorf",
+      "author" => "fritz lang",
+      "text" => "text post 3"
+    ]
+  ]
+];
+
+var_dump($movies);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+
+    for($i = 0; $i < count($movies); $i++){
+      echo $movies[$i];
+    };
+    
+
+  
+  ?>
+</body>
+</html>
